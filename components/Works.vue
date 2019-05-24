575809783080eca626db08c3c5b0c7f8ae00cd29
TatsuroMoriyama <t4traw@gmail.com>
2019-05-24 18:22:07 +0900

    cardだとレイアウトが壊れるので一旦削除

--- components/Works.vue
+++ components/Works.vue
@@ -1,108 +1,13 @@
 <template>
   <section class="container">
     <h2 class="title">今までにやってきたこと</h2>
-    <div class="columns is-vcentered is-multiline">
+    <div class="columns is-vcentered is-multiline is-centerd">
       <div class="column is-4">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="subtitle is-4">John Smith</p>
-              </div>
-            </div>
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            </div>
-          </div>
-        </div>
+        <img src="https://placehold.it/500x300" width="" height="">
+        <h3>foobar</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
+        <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="subtitle is-4">John Smith</p>
-              </div>
-            </div>
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="subtitle is-4">John Smith</p>
-              </div>
-            </div>
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="subtitle is-4">John Smith</p>
-              </div>
-            </div>
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column is-4">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="subtitle is-4">John Smith</p>
-              </div>
-            </div>
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-            </div>
-          </div>
-        </div>
-      </div>
-
     </div>
   </section>
 </template>
@@ -121,17 +26,7 @@ export default {
 
 .columns {
   .column {
-    h3 {
-      font-size: 1.2rem;
-      font-weight: bold;
-      margin: 2rem 0 1rem;
-    }
-    img {
-    }
-    p {
-      text-align: left;
-      margin: 1rem 0;
-    }
+
   }
 }
 </style>
