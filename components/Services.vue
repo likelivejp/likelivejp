e751c0ed5317cc1137886c6f5500d2fdf5fee729
TatsuroMoriyama <t4traw@gmail.com>
2019-06-03 13:46:23 +0900

    サービスのタイトルを変更

--- components/Services.vue
+++ components/Services.vue
@@ -3,39 +3,39 @@
     <div class="columns">
       <div class="column">
         <img src="~assets/images/development.svg" width="" height="">
-        <h3>foobar</h3>
+        <h3>IT戦略・提案</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
         <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
       <div class="column">
-        <img src="~assets/images/it.svg" width="" height="">
-        <h3>foobar</h3>
+        <img src="~assets/images/clipboard.svg" width="" height="">
+        <h3>経営やサイト相談</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
         <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
       <div class="column">
-        <img src="~assets/images/automation.svg" width="" height="">
-        <h3>foobar</h3>
+        <img src="~assets/images/status.svg" width="" height="">
+        <h3>WEBプロデュース</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
         <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
     </div>
     <div class="columns">
       <div class="column">
-        <img src="~assets/images/status.svg" width="" height="">
-        <h3>foobar</h3>
+        <img src="~assets/images/it.svg" width="" height="">
+        <h3>運用サポート</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
         <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
       <div class="column">
-        <img src="~assets/images/clipboard.svg" width="" height="">
-        <h3>foobar</h3>
+        <img src="~assets/images/automation.svg" width="" height="">
+        <h3>業務効率化・自動化</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
         <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
       <div class="column">
         <img src="~assets/images/coding.svg" width="" height="">
-        <h3>foobar</h3>
+        <h3>サイト制作・開発</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit dolores</p>
         <nuxt-link to="/" class="button is-rounded">くわしく見る</nuxt-link>
       </div>
