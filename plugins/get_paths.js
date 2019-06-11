221e75176c643ab43106881208c1abc3e7237742
TatsuroMoriyama <t4traw@gmail.com>
2019-06-11 10:21:47 +0900

    記事一覧を降順に変更

--- plugins/get_paths.js
+++ plugins/get_paths.js
@@ -4,7 +4,7 @@ import { sourceFileArray } from '../.tmp/summary.json';
 
 Vue.prototype.$getPaths = (cat) => {
   let filteredArray = sourceFileArray.filter(v => v.split('/')[1]===cat)
-  return filteredArray.map(sourceFileName => {
+  return filteredArray.reverse().map(sourceFileName => {
     const deleteExt = sourceFileName.replace('.md', '')
     let splited = deleteExt.split("/")
     splited.shift()
