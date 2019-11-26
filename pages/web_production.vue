7e4913d4db4428f80f696814c15a2ce6c487ccbf
TatsuroMoriyama <t4traw@gmail.com>
2019-11-26 11:11:21 +0900

    ウェブサイト料金ページのラフ作成

--- pages/web_production.vue
+++ pages/web_production.vue
@@ -8,13 +8,11 @@
             <img src="~assets/images/2984354.png" alt="" />
           </div>
           <div class="column">
+            <p>お客様のビジネスを理解した最適なウェブサイト・アプリケーションの制作・開発。</p>
             <p>
-              お客様のビジネスを理解した最適なウェブサイト・アプリケーションの制作・開発。
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia alias numquam ipsum inventore. Iste
-              fugiat perferendis incidunt placeat magni exercitationem repellat quas voluptatem voluptates tempora,
-              dolores aliquam obcaecati nulla veniam.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quia aut illum perferendis nemo. Voluptas
+              tenetur magni corporis id, earum exercitationem eaque, cumque, dolorum facilis reprehenderit laudantium
+              aliquam nostrum debitis!
             </p>
             <a href="/contact" class="button is-rounded has-text-weight-bold">すぐにお問い合わせ</a>
           </div>
@@ -57,54 +55,90 @@
         </div>
       </section>
       <section>
-        <h2 class="title">料金シミュレーター</h2>
-        <table class="table is-fullwidth">
-          <tbody>
-            <tr>
-              <td><img src="~assets/images/startup.svg" alt="" class="option_icon" /></td>
-              <td>基本料金</td>
-              <td>150,000円</td>
-              <td><input type="checkbox" checked="checked" disabled /></td>
-            </tr>
-            <tr v-for="item in items">
-              <td><img :src="`/images/${item.image}.svg`" alt="" class="option_icon" /></td>
-              <td>{{ item.name }}</td>
-              <td>{{ item.price }}</td>
-              <td><input type="checkbox" v-model="item.checked" /></td>
-            </tr>
-          </tbody>
-        </table>
-
-        <span>合計金額: {{ totalPrice }}</span>
+        <div class="columns is-centered">
+          <div class="column is-8">
+            <h2 class="title">料金シミュレーター</h2>
+            <table class="table is-fullwidth is-narrow">
+              <tbody>
+                <tr></tr>
+                <tr v-for="item in basicItems">
+                  <td><img :src="`/images/${item.image}.svg`" alt="" class="option_icon" /></td>
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.price.toLocaleString() }}円</td>
+                  <td>
+                    <input type="checkbox" v-model="item.checked" class="checkbox" disabled />
+                    <input type="checkbox" :name="item.name" class="hidden" />
+                  </td>
+                </tr>
+                <tr v-for="item in optionItems">
+                  <td><img :src="`/images/${item.image}.svg`" alt="" class="option_icon" /></td>
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.price.toLocaleString() }}円</td>
+                  <td><input type="checkbox" v-model="item.checked" class="checkbox" /></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="is-centered">
+          <div class="price_board">
+            合計金額:
+            <span>{{ totalPrice }}</span>
+            円
+          </div>
+        </div>
+      </section>
+      <section>
+        <contact />
+        <p>{{ optionItems }}</p>
       </section>
     </div>
   </section>
 </template>
 
 <script>
-const items = [
-  { name: '技術的相談・ご提案', price: 1200, image: 'advise', checked: false },
-  { name: 'オプションB', price: 1400, image: '', checked: false }
+import Contact from '~/components/Contact.vue'
+
+const basicItems = [
+  { name: '基本制作料金', price: 200000, image: 'startup', checked: true },
+  { name: 'スマートフォン対応', price: 0, image: 'smartphone', checked: true },
+  { name: 'セキュリティ対応', price: 0, image: 'security', checked: true },
+  { name: 'お問い合わせフォーム', price: 0, image: 'web-design', checked: true },
+  { name: '技術相談・提案', price: 0, image: 'advise', checked: true }
+]
+const optionItems = [
+  { name: '独自ドメイン対応', price: 12345, image: 'domain', checked: false },
+  { name: 'ブログ機能', price: 12345, image: 'blog', checked: false },
+  { name: 'SNS連携', price: 12345, image: 'sharing', checked: false },
+  { name: '購入ボタン設置', price: 12345, image: 'cart', checked: false },
+  { name: 'LINE@', price: 12345, image: 'line', checked: false },
+  { name: 'Googleビジネス', price: 12345, image: 'google', checked: false },
+  { name: '広告マーケティング', price: 12345, image: 'marketing', checked: false },
+  { name: 'キーワードリサーチ', price: 12345, image: 'search', checked: false }
 ]
 
 export default {
+  components: {
+    Contact
+  },
   data() {
     return {
       basicPrice: 150000,
-      items: items
+      basicItems: basicItems,
+      optionItems: optionItems
     }
   },
   computed: {
     totalPrice: function() {
       return (
-        this.items.reduce(function(sum, item) {
+        this.optionItems.reduce(function(sum, item) {
           if (item.checked === true) {
             return sum + item.price
           } else {
             return sum
           }
         }, 0) + this.basicPrice
-      )
+      ).toLocaleString()
     }
   }
 }
@@ -130,6 +164,116 @@ p {
 }
 
 .option_icon {
-  width: 75px;
+  width: 50px;
+}
+
+table {
+  tbody {
+    tr {
+      td {
+        @include font-size(1.1rem);
+        text-align: center;
+        vertical-align: middle;
+        &:nth-of-type(2) {
+          text-align: left;
+        }
+        &:nth-of-type(3) {
+          text-align: right;
+        }
+      }
+    }
+  }
+}
+
+@keyframes click-wave {
+  0% {
+    position: relative;
+    width: 30px;
+    height: 30px;
+    opacity: 0.35;
+  }
+  100% {
+    width: 200px;
+    height: 200px;
+    margin-top: -80px;
+    margin-left: -80px;
+    opacity: 0;
+  }
+}
+
+.checkbox {
+  $checkbox_color: #67d4b2;
+  position: relative;
+  top: 3px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 30px;
+  height: 30px;
+  margin-right: 0.5rem;
+  cursor: pointer;
+  transition: all 0.15s ease-out 0s;
+  color: #ffffff;
+  border: none;
+  outline: none;
+  background: lighten($color: desaturate($checkbox_color, 50), $amount: 28);
+  appearance: none;
+  &:hover {
+    background: lighten($color: $checkbox_color, $amount: 15);
+  }
+  &:checked {
+    background: $checkbox_color;
+  }
+  &:checked::before {
+    font-size: 20px;
+    line-height: 30px;
+    position: absolute;
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    content: '✔';
+    text-align: center;
+  }
+  &:checked::after {
+    position: relative;
+    display: block;
+    content: '';
+    animation: click-wave 0.65s;
+    background: $checkbox_color;
+  }
+  &.radio {
+    border-radius: 50%;
+  }
+  &.radio::after {
+    border-radius: 50%;
+  }
+  label {
+    line-height: 40px;
+    display: block;
+  }
+  &:disabled {
+    cursor: not-allowed;
+    background: $checkbox_color;
+  }
+  &:disabled::before {
+    font-size: 20px;
+    line-height: 30px;
+    position: absolute;
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    content: '✔';
+    text-align: center;
+  }
+}
+
+.price_board {
+  background-color: #ffd587;
+  padding: 1rem 2rem;
+  font-size: 1.6rem;
+  display: inline-block;
+  span {
+    font-weight: bold;
+  }
 }
 </style>
