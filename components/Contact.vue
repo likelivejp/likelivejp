8800db93df854aee8bc8721bc38c80a916078cdc
TatsuroMoriyama <t4traw@gmail.com>
2019-11-28 10:45:02 +0900

    Drop formrun

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="mail_form">
     <h2 class="title is-centered">お問い合わせ</h2>
-    <script src="https://www.google.com/recaptcha/api.js"></script>
-    <script src="https://sdk.form.run/js/v2/formrun.js"></script>
-    <form class="formrun" action="https://form.run/api/v1/r/71dsyybzgy9kj3lu3vmiptft" method="post">
+    <form class="formrun" action="/mailform" method="post">
       <div class="field">
         <label class="label">お名前<span class="required"> *必須</span></label>
         <input name="お名前" type="text" class="text input" />
@@ -11,8 +9,7 @@
 
       <div class="field">
         <label class="label">メールアドレス<span class="required"> *必須</span></label>
-        <input name="メールアドレス" class="text input" type="text" data-formrun-type="email" data-formrun-required />
-        <div data-formrun-show-if-error="メールアドレス">メールアドレスを正しく入力してください</div>
+        <input name="メールアドレス" class="text input" type="text" />
       </div>
 
       <div class="field">
@@ -21,31 +18,10 @@
 
       <div class="field">
         <label class="label">お問い合わせ<span class="required">*必須</span></label>
-        <textarea name="お問い合わせ" class="textarea" data-formrun-required></textarea>
-        <div data-formrun-show-if-error="お問い合わせ">お問い合わせ内容を入力してください</div>
+        <textarea name="お問い合わせ" class="textarea"></textarea>
       </div>
 
-      <div class="_formrun_gotcha">
-        <style media="screen">
-          ._formrun_gotcha {
-            position: absolute !important;
-            height: 1px;
-            width: 1px;
-            overflow: hidden;
-          }
-        </style>
-        <label for="_formrun_gotcha">If you are a human, ignore this field</label>
-        <input type="text" name="_formrun_gotcha" id="_formrun_gotcha" tabindex="-1" />
-      </div>
-
-      <div class="g-recaptcha" data-sitekey="6Lcy_b8UAAAAAK7j1ZIhK7Q4bVzkKyW6Pxhxk_Dr"></div>
-
-      <button
-        type="submit"
-        class="button is-primary has-text-weight-bold"
-        data-formrun-error-text="未入力の項目があります"
-        data-formrun-submitting-text="送信中..."
-      >
+      <button type="submit" class="button is-primary has-text-weight-bold">
         送信
       </button>
     </form>
